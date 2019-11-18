70ca6afe0d9485b1d0a660f4d8fe1284979f63de
Amol Tangade <amoltangade@dbatu.ac.in>
2019-11-19 02:04:24 +0530

    Wrapper Component
    Wrapper component to place the children at the center.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 import ExploreButton from "./components/ExploreButton";
 
-import "./styles.css";
+const color = "#111";
+
+const Wrapper = styled.div`
+  height: auto;
+  display: flex;
+  align-items: center;
+  margin-top: 50%;
+  font-family: Avenir, sans-serif;
+  color: ${color};
+`;
 
 function App() {
   return (
     <div className="App">
-      <div style={{ height: "100%", display: "grid" }}>
+      <Wrapper>
         <ExploreButton />
-      </div>
+      </Wrapper>
     </div>
   );
 }
